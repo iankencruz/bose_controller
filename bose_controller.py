eafd1fb75387f595ed5d244d223a34d6cf83a2f6
Ian Dela Cruz <iankencruz@gmail.com>
2021-10-04 20:34:24 +1100

    basic ui for bose controls

--- bose_controller.py
+++ bose_controller.py
@@ -1,13 +1,17 @@
 import sys
 import random 
+
+
 from PySide6 import QtCore, QtWidgets, QtGui
 from libsoundtouch import device, soundtouch_device
-
 
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+
+
+        self.setWindowTitle("Bose Controller")
         
         self.device = soundtouch_device('10.1.1.21')  # Manual configuration
         # self.device.select_source_aux()
@@ -15,48 +19,67 @@ class MyWidget(QtWidgets.QWidget):
 
         self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
 
-        self.button_plus = QtWidgets.QPushButton("+")
-        self.button_minus = QtWidgets.QPushButton("-")
+        self.power_on_btnn = QtWidgets.QPushButton("Power On")
+        self.power_off_bttn = QtWidgets.QPushButton("Power Off")
+
+
+
+
+        self.vol_slider = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal, self)
+        self.vol_slider.setGeometry(10, 60, 280, 30)
+        self.vol_slider.setValue(self.volume.actual)
+        self.vol_slider.valueChanged[int].connect(self.changeValue)
+
+        
         self.text = QtWidgets.QLabel(str(self.volume.actual),
                                      alignment=QtCore.Qt.AlignCenter)
+        self.text.setFont(QtGui.QFont('Poppins', 20))
 
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button_plus)
-        self.layout.addWidget(self.button_minus)
+        self.layout_top = QtWidgets.QHBoxLayout(self)
+        self.layout_top.addWidget(self.text)
+        self.layout_top.addWidget(self.power_on_btnn)
+        self.layout_top.addWidget(self.power_off_bttn)
 
-        self.button_plus.clicked.connect(self.volumeUp)
-        self.button_minus.clicked.connect(self.volumeDown)
+
+
+
+        self.layout_bot = QtWidgets.QHBoxLayout(self)
+
+        self.layout_bot.addWidget(self.vol_slider)
+
+
+        self.layout_top.setAlignment(QtCore.Qt.AlignTop)
+        self.layout_bot.setAlignment(QtCore.Qt.AlignTop)
+
+
+        self.power_on_btnn.clicked.connect(self.powerOn)
+        self.power_off_bttn.clicked.connect(self.powerOff)
 
     @QtCore.Slot()
-    def volumeUp(self):
-        self.volume = self.device.volume()
-        curr_vol = self.volume.actual
-        new_vol = curr_vol + 5 
-        self.device.set_volume(new_vol)
-        self.text.setText(str(self.volume.actual))
+    def powerOn(self):
+        self.device.power_on()
 
     @QtCore.Slot()
-    def volumeDown(self):
-        self.volume = self.device.volume()
-        curr_vol = self.volume.actual
-        new_vol = curr_vol - 5 
-        self.device.set_volume(new_vol)
-        self.text.setText(str(self.volume.actual))
+    def powerOff(self):
+        self.device.power_off()
 
-    # def plusVol(self, a):
-    #     curr_vol = self.device.volume()
-    #     curr_vol += a
-    #     self.device.set_volume(new_vol)
+
+    def changeValue(self, value):
+
+        self.volume = self.device.volume()
+        self.device.set_volume(self.vol_slider.value())
+        self.text.setText(str(self.vol_slider.value()))
 
 
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
+    # app = QtWidgets.QApplication([])
+    app = QtWidgets.QApplication(sys.argv)
+    
 
     widget = MyWidget()
-    widget.resize(800, 600)
+    widget.resize(300, 100)
     widget.show()
 
     sys.exit(app.exec())
